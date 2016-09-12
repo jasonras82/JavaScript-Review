c63e1f9406d974dd2cc5f576419fbf39ecf9bfad
jasonsanders <jas.sanders@gmail.com>
2016-09-12 15:59:27 -0600

    closures done-ish

--- closureReview.js
+++ closureReview.js
@@ -6,11 +6,12 @@ var callFriend = function(){
   return callF;
 };
 
-//Above you're given a callFriend function that returns another function. 
+//Above you're given a callFriend function that returns another function.
 //Do what you need to do in order to call your function and get 'Calling Jake at 435-215-9248' in your console.
 
   //code here
-
+var callStr = callFriend();
+console.log(callStr("801-636-5595"));
 
 
 /*
@@ -20,3 +21,31 @@ Write a function that accepts a function as it's first argument and returns a ne
 Once completed, add a second arguments that allows the function to be executed N number of times. After the function has been called N number of times, console.log('STAHHP');
 
 */
+
+//////////////////////////////// Version 1
+function acceptFn() {
+  var counter = 0;
+  return function (x) {
+    for(var i = 0; i < x; i++) {
+      counter++;
+    }console.log(counter);
+    console.log('STAHHP');
+  };
+}
+
+var runFn = acceptFn();
+runFn(5); // Prints --> 5 / STAHHP
+
+//////////////////////////////// Version 2
+function acceptFn(cb) {
+  var counter = 0;
+  return function cb(x) {
+    for(var i = 0; i < x; i++) {
+      counter++;
+    }console.log(counter);
+    console.log('STAHHP');
+  };
+}
+
+var runFn = acceptFn(runFn);
+runFn(5); // Prints --> 5 / STAHHP
